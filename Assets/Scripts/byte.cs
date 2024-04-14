26287a9534b3bd0f2983e35c9907bd3fee924fa1
Brian David Moreno <70359461+2201brian@users.noreply.github.com>
2024-04-14 02:32:23 -0500

    Initial movement to byte

---
+++ Assets/Scripts/byte.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] private float speed = 2f;
+    [SerializeField] private float turnSpeed = 45f;
+
+    private Animator animator;
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var velocity = Vector3.forward * Input.GetAxis("Vertical")* speed;
+        transform.Translate(velocity * Time.deltaTime * 2);
+        transform.Rotate(Vector3.up, Input.GetAxis("Horizontal") * Time.deltaTime * turnSpeed);
+        animator.SetFloat("Speed", velocity.z);
+    }
+}
